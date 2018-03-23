ab4e304725bb323358ac1536d6ba19e6b6ae8ca6
JoTaDeusz <37026593+JoTaDeusz@users.noreply.github.com>
2018-03-23 19:39:12 -0300

    Add files via upload
    Adicionei manualmente porque não consegui commitar nem pushar

--- src/ccc_lib.c
+++ src/ccc_lib.c
@@ -2,12 +2,30 @@
 #include "ccc_lib.h"
 
 void camelcase_to_underline(char *input, char *output) {
-
+  int i, k = 1;  
+  output[0] = input[0] + 32;
+  for(i = 1; input[i] != '\0'; i++){
+    if((input[i] <= 90) && (input[i] >= 65)){ output[k] = '_'; k++; output[k] = input[i] + 32; }
+    else output[k] = input[i];
+    k++;
+    }
+  
   return;
 }
 
 void underline_to_camelcase(char *input, char *output) {
-
+  int i, k = 1;  
+  output[0] = input[0] - 32;
+  
+  for(i = 1; input[k] != '\0'; i++){
+    
+    if(input[k] == '_'){ k++; output[i] = input[k] - 32; }
+    else  output[i] = input[k];  
+    
+     k++;
+    
+  }
+  
   return;
 }
 
